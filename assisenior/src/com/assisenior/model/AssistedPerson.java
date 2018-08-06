c77ccf8ad4b1a2d279b7786a0e3e8eeef26d8f0a
Absink <olivier.blaivie@gmail.com>
2018-08-06 19:19:44 +0200

    Update

--- assisenior/src/com/assisenior/model/AssistedPerson.java
+++ assisenior/src/com/assisenior/model/AssistedPerson.java
@@ -20,7 +20,7 @@ import javax.persistence.Table;
 public class AssistedPerson implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Iddf g 
+	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
